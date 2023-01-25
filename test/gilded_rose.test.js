36f3b3c18b0120d893698a445153e492b0025e1e
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-25 14:37:13 +0000

    refactored initial feature test to pass when one item added updatedQuality once

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,11 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function () {
-  it("should foo", function () {
+describe("adds item and updates sellIn and quality", () => {
+  it("when item is foo with 0 sellIn and 0 quality", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(0);
   });
 });
