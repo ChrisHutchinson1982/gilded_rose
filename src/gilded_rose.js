06cba06e3a5ea365b37812dfb45a79768f18cca1
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-27 13:25:30 +0000

    written test prior to implementing Conjured Mana Cake item rules

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -28,11 +28,11 @@ class Shop {
     } else if (item.name === "Backstage passes to a TAFKAL80ETC concert") {
       this.updateBackStageQuality(item);
     } else {
-      this.updateStandardQuality(item);
+      this.updateNormalQuality(item);
     }
   }
 
-  updateStandardQuality(item) {
+  updateNormalQuality(item) {
     if (item.quality > 0) {
       this.runQualityRules(item, -1, -1, -1, -2);
       this.defaultToMinValue(item);
